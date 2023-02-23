0414a606ef211de27b2914509ca4323846e1b199
Unixan <115973884+Unixan@users.noreply.github.com>
2023-02-23 20:24:33 +0100

    WIP

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <h2>Refs</h2>
+    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+    <button @click="updateNinjaOne">Update ninja one</button>
+    <h2>Reactive</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+    <button @click="updateNinjaTwo">Update ninja two</button>
+  </div>
+</template>
+
+<script>
+import { ref, reactive } from "vue";
+export default {
+  name: "Home",
+  setup() {
+    const ninjaOne = ref({ name: "Mario", age: 30 });
+    const ninjaTwo = reactive({ name: "Luigi", age: 35 });
+    const updateNinjaOne = () => {
+      ninjaOne.value.age = 40;
+    };
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 40;
+    };
+    return { ninjaOne, ninjaTwo, updateNinjaOne, updateNinjaTwo };
+  },
+};
+</script>
