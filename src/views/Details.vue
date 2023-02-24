b7b11527035518da6a5e492c11c3c1112f0f9021
Unixan <115973884+Unixan@users.noreply.github.com>
2023-02-24 16:52:13 +0100

    Next stop: firebase

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,18 +1,26 @@
 <template>
-    <div v-if="error">{{ error }}</div>
-    <div v-if="post" class="post">
+  <div v-if="error">{{ error }}</div>
+  <div v-if="post" class="post">
     <h3>{{ post.title }}</h3>
     <p class="pre">{{ post.body }}</p>
-    </div>
+  </div>
+  <div v-else>
+    <Spinner />
+  </div>
 </template>
 
 <script>
 import getPost from "../composables/getPost.js";
+import Spinner from "../components/Spinner.vue";
+import { useRoute } from "vue-router";
 
 export default {
+  components: { Spinner },
   props: ["id"],
   setup(props) {
-    const { post, error, load } = getPost(props.id);
+    const route = useRoute();
+
+    const { post, error, load } = getPost(route.params.id);
 
     load();
 
@@ -22,18 +30,18 @@ export default {
 </script>
 
 <style>
-.post{
-    max-width: 1200px;
-    margin: 0 auto;
+.post {
+  max-width: 1200px;
+  margin: 0 auto;
 }
 
-.post p{
-    color: #444;
-    line-height: 1.5em;
-    margin-top: 40px;
+.post p {
+  color: #444;
+  line-height: 1.5em;
+  margin-top: 40px;
 }
 
-.pre{
-    white-space: pre-wrap;
+.pre {
+  white-space: pre-wrap;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,24 @@
   <div class="home">
     <h1>Home</h1>
     <div v-if="error">{{ error }}</div>
-    <div v-if="posts.length">
+    <div v-if="posts.length" class="layout">
       <PostList :posts="posts" />
+      <TagCloud :posts="posts" />
     </div>
-    <div v-else>Loading ...</div>
+    <div v-else>
+      <spinner />
+    </div>
   </div>
 </template>
 
 <script>
 import PostList from "../components/PostList.vue";
+import TagCloud from "../components/TagCloud.vue";
 import getPosts from "../composables/getPosts.js";
+import Spinner from "../components/Spinner.vue";
 export default {
   name: "Home",
-  components: { PostList },
+  components: { PostList, Spinner, TagCloud },
   setup() {
     const { posts, error, load } = getPosts();
 
@@ -24,3 +29,16 @@ export default {
   },
 };
 </script>
+<style>
+.home {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 10px;
+}
+
+.layout {
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  gap: 20px;
+}
+</style>
